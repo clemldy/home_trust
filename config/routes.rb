ca47bc19849ca084418acf846290972579dd0d3f
clement ledey <clement.ledey@gmail.com>
2015-12-19 12:49:07 +0100

    rails_apps_composer: set up mail_view for development

--- config/routes.rb
+++ config/routes.rb
@@ -23,31 +23,31 @@ Rails.application.routes.draw do
   #     member do
   #       get 'short'
   #       post 'toggle'
-  #     end
+  #     mount MailPreview => 'mail_view' if Rails.env.development?end
   #
   #     collection do
   #       get 'sold'
-  #     end
-  #   end
+  #     mount MailPreview => 'mail_view' if Rails.env.development?end
+  #   mount MailPreview => 'mail_view' if Rails.env.development?end
 
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
-  #   end
+  #   mount MailPreview => 'mail_view' if Rails.env.development?end
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
   #     resources :sales do
   #       get 'recent', on: :collection
-  #     end
-  #   end
+  #     mount MailPreview => 'mail_view' if Rails.env.development?end
+  #   mount MailPreview => 'mail_view' if Rails.env.development?end
 
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
-  #   end
+  #   mount MailPreview => 'mail_view' if Rails.env.development?end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
 
@@ -56,5 +56,5 @@ Rails.application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  #   end
-end
+  #   mount MailPreview => 'mail_view' if Rails.env.development?end
+mount MailPreview => 'mail_view' if Rails.env.development?end
